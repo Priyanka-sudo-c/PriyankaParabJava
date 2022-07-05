bac48ad907ecac5c12be19396d4aca0a624d10a0
Priyanka-sudo-c <84138246+Priyanka-sudo-c@users.noreply.github.com>
2022-07-05 11:19:33 +0530

    Add files via upload

---
+++ ConstruMainInh.java
@@ -0,0 +1,32 @@
+package edu.java.com;
+class A{
+	A(){
+	
+		System.out.println("Class A const");
+	}
+}
+
+class B extends A{
+
+ 
+	B(){
+		
+		System.out.println("Class B Const ");
+	}
+}
+class C extends B{
+	public C() {
+		
+		System.out.println("C class constr");
+		
+	}
+}
+
+public class ConstruMainInh {
+
+	public static void main(String[] args) {
+		
+       A cob=new A();
+	}
+
+}
