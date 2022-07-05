d53ce42539292c56e7d051dc1da8d0c149d25d34
Priyanka-sudo-c <84138246+Priyanka-sudo-c@users.noreply.github.com>
2022-07-05 11:26:37 +0530

    Add files via upload

---
+++ VolumeOfSphere.java
@@ -0,0 +1,29 @@
+package com.edu.over;
+
+class VolumeOverloading{
+	double volume(double r) {
+        double vol = (4/3)*(22/7)*(r*r*r);
+		return vol;
+	}
+	double volume(double h, double r) {
+		double v=(22/7)*(r*r)*h;
+		return v;
+	}
+	double volume(double l, double b, double h) {
+		//double cv=l*b*h;
+		return l*b*h;
+	}
+}
+
+public class VolumeOfSphere {
+
+	public static void main(String[] args) {
+		VolumeOverloading vob=new VolumeOverloading();
+		//double v=vol.volume(4);
+		//System.out.println("Volume of a sphere is "+v);
+		System.out.println("Volume of a sphere is "+vob.volume(4));
+		System.out.println("Volume of cylinder is "+vob.volume(4, 8));
+		System.out.println("Volume of cuboid is "+vob.volume(2, 8, 9));
+	}
+
+}
