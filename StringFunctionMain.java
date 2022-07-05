e94b90af4bfa9383f0827f26316f6cb682b1e672
Priyanka-sudo-c <84138246+Priyanka-sudo-c@users.noreply.github.com>
2022-07-05 11:49:33 +0530

    Add files via upload

---
+++ StringFunctionMain.java
@@ -0,0 +1,15 @@
+package com.edu;
+
+public class StringFunctionMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//index->position(1->r)
+		//LastIndex(r->1)
+ String s="hello 67i23";
+//01234
+System.out.println("index of l"+s.indexOf("h1"));//2
+System.out.println("lastIndexOf l"+s.lastIndexOf('1'));//3
+	}
+
+}
