36e7e3cd5a2f8881e85b108e566201229fe236dd
Priyanka-sudo-c <84138246+Priyanka-sudo-c@users.noreply.github.com>
2022-07-05 11:51:38 +0530

    Add files via upload

---
+++ DoWhileLoop.java
@@ -0,0 +1,14 @@
+package edu.com;
+import java.util.Scanner;
+public class DoWhileLoop {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method
+int i=10;
+do {
+	System.out.println("The number is: "+i);
+    i++;
+      }while(i<=20);
+     }
+     }
